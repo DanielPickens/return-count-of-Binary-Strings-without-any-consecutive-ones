5b29fb310013edba9a0af55a4e869336ccbc6e62
Dan Pickens <72703981+DanielPickens@users.noreply.github.com>
2020-12-27 02:11:41 -0700

    Create Pythonsolution.py

---
+++ Pythonsolution.py
@@ -0,0 +1,29 @@
+def countStrings(n): 
+
+  
+
+    a=[0 for i in range(n)] 
+
+    b=[0 for i in range(n)] 
+
+    a[0] = b[0] = 1
+
+    for i in range(1,n): 
+
+        a[i] = a[i-1] + b[i-1] 
+
+        b[i] = a[i-1] 
+
+      
+
+    return a[n-1] + b[n-1] 
+
+  
+# this program will allow for testing of above function.
+
+  
+
+print(countStrings(3))
+
+
+Output: 5
